48b50df7f4003584e2f0baee1a084f6189472b60
Nghia_ne <nghialph2002@gmail.com>
2024-04-10 09:02:31 +0700

    4-10

---
+++ test/sample.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <Windows.h>
+#include <Urlmon.h>
+
+using namespace std;
+
+int main()
+{
+    cout <<"hello, this excutable file stand for an virus file";
+    return 1;
+}
